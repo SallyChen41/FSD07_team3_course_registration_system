01794c87bc4bfa98dca56de2c0686b47be36a82b
SiyiC41 <csyclara0122@gmail.com>
2023-05-03 12:02:57 -0400

    Final Project Build

---
+++ CourseRegistrationSystem/src/test/java/com/fsd07team3/CourseRegistrationSystem/CourseRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsd07team3.CourseRegistrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
