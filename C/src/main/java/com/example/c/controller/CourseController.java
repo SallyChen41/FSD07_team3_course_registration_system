5d51f64f8b8a94bc52f7aafe832f6cd499c018ed
jean9947 <jean7499@gmail.com>
2023-05-09 13:11:51 -0400

    5.1.3

--- C/src/main/java/com/example/c/controller/CourseController.java
+++ C/src/main/java/com/example/c/controller/CourseController.java
@@ -2,11 +2,14 @@ package com.example.c.controller;
 
 import com.example.c.entity.Course;
 import com.example.c.entity.Semester;
+import com.example.c.entity.StudentRegistration;
 import com.example.c.entity.User;
 import com.example.c.repository.CourseRepository;
 import com.example.c.repository.SemesterRepository;
+import com.example.c.repository.StudentRegistrationRepository;
 import com.example.c.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,7 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class CourseController {
@@ -26,6 +29,8 @@ public class CourseController {
     private UserRepository userRepo;
     @Autowired
     private SemesterRepository semesterRepo;
+    @Autowired
+    private StudentRegistrationRepository studentRegistrationRepo;
 
 
     /******************* Admin *******************/
@@ -34,8 +39,17 @@ public class CourseController {
     @GetMapping("/admin/courses")
     public String showCourses(Model model) {
         List<Course> courses = courseRepo.findAll();
+        // Get available spots for each course
+        for (Course course : courses) {
+            int registeredStudents = course.getRegistrations().size();
+            int availableSpots = course.getStudentLimit() - registeredStudents;
+            if (availableSpots < 0) {
+                availableSpots = 0;
+            }
+            course.setAvailable(availableSpots);
+        }
         model.addAttribute("courses", courses);
-        return "coursesList";
+        return "admin_list_courses";
     }
 
     // ADD course
@@ -44,16 +58,15 @@ public class CourseController {
         model.addAttribute("course", new Course());
         List<Semester> semesters = semesterRepo.findAll();
         model.addAttribute("semesters", semesters);
-//        List<User> instructors = userRepo.findByRole(User.Role.INSTRUCTOR);
         List<User> instructors = userRepo.findByRole("INSTRUCTOR");
         model.addAttribute("instructors", instructors);
-        return "addCourse";
+        return "admin_courseAdd_form";
     }
     @PostMapping("/admin/courses/add")
     public String addCourse(@ModelAttribute("course") @Valid Course course, BindingResult result,
                             RedirectAttributes redirectAttributes) {
         if (result.hasErrors()) {
-            return "addCourse";
+            return "admin_courseAdd_form";
         }
         courseRepo.save(course);
         redirectAttributes.addFlashAttribute("message", "Course '" + course.getTitle() + "' added.");
@@ -67,16 +80,15 @@ public class CourseController {
         model.addAttribute("course", course);
         List<Semester> semesters = semesterRepo.findAll();
         model.addAttribute("semesters", semesters);
-//        List<User> instructors = userRepo.findByRole(User.Role.INSTRUCTOR);
         List<User> instructors = userRepo.findByRole("INSTRUCTOR");
         model.addAttribute("instructors", instructors);
-        return "editCourse";
+        return "admin_courseEdit_form";
     }
     @PostMapping("/admin/courses/edit")
     public String editCourse(@ModelAttribute("course") @Valid Course course,
                              BindingResult result, RedirectAttributes redirectAttributes) {
         if (result.hasErrors()) {
-            return "editCourse";
+            return "admin_courseEdit_form";
         }
         courseRepo.save(course);
         redirectAttributes.addFlashAttribute("message", "Course '" + course.getTitle() + "' updated.");
@@ -96,7 +108,6 @@ public class CourseController {
         try {
             Course course = courseRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("Course not found with ID " + id));
             courseRepo.deleteById(id);
-            redirectAttributes.addFlashAttribute("message", "Course '" + course.getTitle() + "' deleted.");
         } catch (EntityNotFoundException ex) {
             redirectAttributes.addFlashAttribute("error", ex.getMessage());
         }
@@ -104,47 +115,121 @@ public class CourseController {
     }
 
 
+    // SHOW courseRegistration list
+    @GetMapping("/admin/studentregistrations")
+    public String showStudentRegistrations(Model model) {
+        List<StudentRegistration> studentRegistrations = studentRegistrationRepo.findAll();
+        model.addAttribute("studentRegistrations", studentRegistrations);
+        return "admin_list_registrations";
+    }
+
+
 
     /******************* Instructor *******************/
 
     // Show list of courses
-    @GetMapping("/instructor/{id}") // id = user.id
-    public String showInstructorCourseList(@PathVariable("id") Long id, Model model) {
-//        List<User> instructors = userRepo.findByRole(User.Role.INSTRUCTOR);
-        List<User> instructors = userRepo.findByRole("INSTRUCTOR");
-        model.addAttribute("instructors", instructors);
-        User instructor = userRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        List<Course> courses = courseRepo.findByInstructor(instructor);
+    @GetMapping("/instructor/courses")
+    public String showInstructorCourse(Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
+        String username = authentication.getName();
+        User user = userRepo.findByUsername(username);
+        List<Course> courses = courseRepo.findByInstructor(user);
+        model.addAttribute("instructors", user);
         model.addAttribute("courses", courses);
-        return "instructorListOfCourses";
+        // Get available spots for each course
+        for (Course course : courses) {
+            int registeredStudents = course.getRegistrations().size();
+            int availableSpots = course.getStudentLimit() - registeredStudents;
+            if (availableSpots < 0) {
+                availableSpots = 0;
+            }
+            course.setAvailable(availableSpots);
+        }
+        redirectAttributes.addFlashAttribute("message", "You're logged in! Professor " + user.getFirstName());
+        return "instructor_list_courses";
     }
-
-
-    // ** a BETTER way using authentication??
-//    @GetMapping("/instructor/courses")
-//    public String showCourseList(Model model, Authentication authentication) {
-//        String username = authentication.getName(); // Retrieve the currently authenticated user's username
-//        User user = userRepo.findByUsername(username); // Retrieve the User object for the currently authenticated user
-//        List<User> instructors = userRepo.findByRole(User.Role.INSTRUCTOR);
-//        model.addAttribute("instructors", instructors);
-//        model.addAttribute("message", "Welcome back " + user.getFirstName()); // Concatenate the user's first name with the welcome message
-//        return "instructorListOfCourses";
-//    }
-
 
 
     /******************* Student *******************/
 
-    // Show list of courses
-    @GetMapping("/student/{id}") // id = user.id
-    public String showStudentCourseList(@PathVariable("id") Long id, Model model) {
-//        List<User> students = userRepo.findByRole(User.Role.STUDENT);
-        List<User> students = userRepo.findByRole("STUDENT");
-        model.addAttribute("students", students);
-        User student = userRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        List<Course> courses = courseRepo.findByInstructor(student);
+    // Show list of registered courses
+    @GetMapping("/student/courses")
+    public String showStudentCourse(Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
+        String username = authentication.getName();
+        User user = userRepo.findByUsername(username);
+        List<StudentRegistration> registrations = studentRegistrationRepo.findByStudentId(user.getId());
+        List<Course> courses = new ArrayList<>();
+        for (StudentRegistration registration : registrations) {
+            if (registration.getStatus().equals("Registered")) {
+                courses.add(registration.getCourse());
+            }
+        }
         model.addAttribute("courses", courses);
-        return "studentListOfCourses";
+        redirectAttributes.addFlashAttribute("message", "You're logged in! ");
+        return "student_list_courses";
+    }
+
+    // Register courses (ADD)
+    @GetMapping("/student/registercourses")
+    public String showAvailableCourses(Model model, Authentication authentication) {
+        String username = authentication.getName();
+        User user = userRepo.findByUsername(username);
+        List<Course> availableCourses = courseRepo.findAvailableCoursesForStudent(user.getId());
+        // Get available spots for each course
+        for (Course course : availableCourses) {
+            int registeredStudents = course.getRegistrations().size();
+            int availableSpots = course.getStudentLimit() - registeredStudents;
+            if (availableSpots < 0) {
+                availableSpots = 0;
+            }
+            course.setAvailable(availableSpots);
+        }
+        model.addAttribute("courses", availableCourses);
+        List<Semester> semesters = semesterRepo.findAll();
+        model.addAttribute("semesters", semesters);
+        return "student_registerCourses";
+    }
+
+    @PostMapping("/student/registercourses")
+    public String registerCourse(@RequestParam("courseId") Long courseId, Model model, Authentication authentication) {
+        String username = authentication.getName();
+        User student = userRepo.findByUsername(username);
+        Course course = courseRepo.findById(courseId).orElseThrow(() -> new RuntimeException("Invalid course id: " + courseId));
+        StudentRegistration registration = new StudentRegistration();
+        registration.setStudent(student);
+        registration.setCourse(course);
+        registration.setStatus("Registered");
+        studentRegistrationRepo.save(registration);
+        course.decrementAvailable();
+        return "redirect:/student/courses";
+    }
+
+    // DROP the course
+    @GetMapping("/student/courses/drop/{id}")
+    public String dropCourse(@PathVariable("id") Long courseId, Authentication authentication) {
+        String username = authentication.getName();
+        User student = userRepo.findByUsername(username);
+        Course course = courseRepo.findById(courseId).orElseThrow(() -> new RuntimeException("Invalid course id: " + courseId));
+        StudentRegistration registration = studentRegistrationRepo.findByStudentAndCourse(student, course);
+        registration.setStatus("Dropped");
+        studentRegistrationRepo.save(registration);
+        course.setAvailable(course.getAvailable() + 1);
+        courseRepo.save(course);
+        return "redirect:/student/courses/dropped";
+    }
+
+    @GetMapping("/student/courses/dropped")
+    public String showDroppedCourses(Model model, Authentication authentication) {
+        String username = authentication.getName();
+        User user = userRepo.findByUsername(username);
+        List<StudentRegistration> registrations = studentRegistrationRepo.findByStudentId(user.getId());
+        List<Course> droppedCourses = new ArrayList<>();
+        for (StudentRegistration registration : registrations) {
+            if (registration.getStatus().equals("Dropped")) {
+                droppedCourses.add(registration.getCourse());
+            }
+        }
+        model.addAttribute("courses", droppedCourses);
+        return "student_list_droppedCourses";
     }
 
 }
--- C/src/main/java/com/example/c/entity/StudentRegistration.java
+++ C/src/main/java/com/example/c/entity/StudentRegistration.java
@@ -1,4 +1,4 @@
-package com.fsd07team3.CourseRegistrationSystem.entity;
+package com.example.c.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-
 
 @Entity
 @Table(name = "studentRegistrations")
